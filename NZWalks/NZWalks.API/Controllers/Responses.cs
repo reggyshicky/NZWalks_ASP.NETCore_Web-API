395d8ddad1c0e57502e6678370d607ab5f631cad
Priscah Reginah Shikanda <reginahshikanda@gmail.com>
2024-04-28 22:01:06 +0300

    Created three entities, installed entity sql server and tools, Created a dbcontext class

---
+++ NZWalks/NZWalks.API/Controllers/Responses.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace NZWalks.API.Controllers
+{
+    public class Responses
+    {
+        public HttpStatusCode statusCode { get; set; }
+        public string? Message { get; set; }
+        public object? Result { get; set; }
+    }
+}
